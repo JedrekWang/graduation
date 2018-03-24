689ad1274a2809e250bb36354be288aed9b8c769
jedrekwang <jedrekwang@gmail.com>
2018-03-24 13:59:20 +0800

    [id]20180224 [desc]修改了spring boot 默认的静态资源配置

--- src/main/java/com/jedrek/graduation/config/WebMvcConfig.java
+++ src/main/java/com/jedrek/graduation/config/WebMvcConfig.java
@@ -5,12 +5,12 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
-//@Configuration
-//public class WebMvcConfig extends WebMvcConfigurerAdapter {
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        //将所有/static/** 访问都映射到classpath:/static/ 目录下
-//        registry.addResourceHandler("/pdf/**").addResourceLocations("classpath:/static/pdf");
-//    }
-//}
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
