33b9902c9667b777ac877160bef5d31348c12209
jedrekwang <jedrekwang@gmail.com>
2018-03-31 17:27:47 +0800

    [id]20180331 [desc] 增加了DES对称加密功能，使得通过注册信息生成一个动态url

--- src/main/java/com/jedrek/graduation/utils/MailUtil.java
+++ src/main/java/com/jedrek/graduation/utils/MailUtil.java
@@ -16,11 +16,16 @@ import com.sun.mail.util.MailSSLSocketFactory;
 public class MailUtil {
     public static void main(String[] args) throws Exception {
         String to = "1850715048@qq.com";
-        sendMail(to);
+        String userMessage = "Jedrek_1850715048@qq.com_12345";
+        sendMail(to, userMessage);
     }
 
 
-    public static void sendMail(String to) {
+    /**
+     * 向指定的邮箱发送一封激活邮件
+     * @param to 发送地址
+     */
+    public static void sendMail(String to, String userMessage) {
         // 发件人电子邮箱
         String from = "1309898641@qq.com";
         // 指定发送邮件的主机为 smtp.qq.com
@@ -42,7 +47,7 @@ public class MailUtil {
         // 获取默认session对象
         Session session = Session.getDefaultInstance(properties, new Authenticator() {
             public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("xxxxxx@qq.com", "xxxxx"); //发件人邮件用户名、密码
+                return new PasswordAuthentication("xxxxx@qq.com", "xxxxxx"); //发件人邮件用户名、密码
             }
         });
 
@@ -60,16 +65,41 @@ public class MailUtil {
             message.setSubject("Welcome to DocumentHub!");
 
             // 设置消息体
-//            message.setText("welcome to DocuemntHub, Are you ready to get started?");
-            String htmlContent = "Welcome, Are you ready to join us?";
-            String url = "<a href=\"http://www.baidu.com\">Join us!</a>";
-            String leftContent = "Let's rock";
-            message.setContent(htmlContent+url+leftContent, "text/html" );
+            // todo 两个问题，一个应发送一个html页面，激活链接应修改
+            String hash = makeHashCode(userMessage);
+            String html = makeMailContent(to, hash);
+            message.setContent(html, "text/html;charset=utf-8");
             // 发送消息
             Transport.send(message);
             System.out.println("Sent message successfully....from runoob.com");
         } catch (MessagingException mex) {
             mex.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 创建邮件内容
+     * @param to  邮箱地址
+     * @param hash l链接中的动态内容
+     *
+     */
+    public static String makeMailContent(String to, String hash) {
+        String url = String.format("localhost:8088/email=%s&hash=%s", to, hash);
+        String html = "<h2>欢迎你注册DoucmentHub，请点击以下链接来激活全部服务</h2>";
+        String link = String.format("<a href=\"%s\">Join us</a>", url);
+        String end = "<h3>如果此操作非本人，请忽略此封邮件</h3>";
+        return html+link+end;
+    }
+
+    public static String makeHashCode(String message) {
+        String cryptKey = "magiccode";
+        String data = message;
+        try {
+            return DESUtil.encrypt(data, cryptKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("生成注册hash值失败");
         }
     }
 }
